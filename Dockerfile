ec5821a76708cdadb4362743d0ed7d39f2b0923b
Damian <damian022w@wp.pl>
2021-05-22 17:26:48 +0200

    Change project name

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,4 @@ RUN npm run build
 
 FROM nginx:1.17.1-alpine
 COPY nginx.conf /etc/nginx/nginx.conf
-COPY --from=build /usr/src/app/dist/tusi-gui /usr/share/nginx/html
+COPY --from=build /usr/src/app/dist/wcas-gui /usr/share/nginx/html
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tusi-gui'`, () => {
+  it(`should have as title 'wcas-gui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tusi-gui');
+    expect(app.title).toEqual('wcas-gui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('tusi-gui app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('wcas-gui app is running!');
   });
 });
